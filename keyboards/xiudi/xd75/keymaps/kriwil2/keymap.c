5b58e714fc050c3c76f234db7f58f3068993f4de
Aldiantoro Nugroho <kriwil@gmail.com>
2024-08-08 19:06:05 +0700

    move the function keys to the right side

--- keyboards/xiudi/xd75/keymaps/kriwil2/keymap.c
+++ keyboards/xiudi/xd75/keymaps/kriwil2/keymap.c
@@ -25,35 +25,35 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COL] = LAYOUT_ortho_5x15( /* COLEMAK */
-        KC_VOLD, KC_MUTE,  KC_VOLU, KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-        KC_F10,  KC_F11,   KC_F12,  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        KC_F7,   KC_F8,    KC_F9,   KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_F4,   KC_F5,    KC_F4,   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_F1,   KC_F2,    KC_F3,   KC_CAPS,  KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC, KC_ENT, MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  KC_VOLD, KC_MUTE,  KC_VOLU,
+        KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, KC_F10,  KC_F11,   KC_F12,
+        KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_F7,   KC_F8,    KC_F9,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_F4,   KC_F5,    KC_F4,
+        KC_CAPS,  KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC, KC_ENT, MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2, KC_F1,   KC_F2,    KC_F3
     ),
 
     [_QWE] = LAYOUT_ortho_5x15( /* QWERTY */
-        KC_VOLD, KC_MUTE,  KC_VOLU, KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-        KC_F10,  KC_F11,   KC_F12,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_F7,   KC_F8,    KC_F9,   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_F4,   KC_F5,    KC_F4,   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_F1,   KC_F2,    KC_F3,   KC_CAPS,  KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC, KC_ENT,  MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  KC_VOLD, KC_MUTE,  KC_VOLU,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_F10,  KC_F11,   KC_F12,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_F7,   KC_F8,    KC_F9,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_F4,   KC_F5,    KC_F4,
+        KC_CAPS,  KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC, KC_ENT,  MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2, KC_F1,   KC_F2,    KC_F3
     ),
 
     [_LO] = LAYOUT_ortho_5x15( /* FUNCTION */
-        QK_BOOT, DF(_COL), DF(_QWE), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX, KC_DEL,
-        _______, _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_DEL,  KC_END,  KC_PGDN, XXXXXXX, XXXXXXX,
-        _______, _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______,  _______,  KC_CAPS, KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC,  KC_ENT,  MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2
+        QK_BOOT, DF(_COL), DF(_QWE), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,  _______,
+        XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX, KC_DEL,  _______, _______,  _______,
+        XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_DEL,  KC_END,  KC_PGDN, XXXXXXX, XXXXXXX, _______, _______,  _______,
+        XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,  _______,
+        KC_CAPS, KC_DEL,   KC_LALT,  KC_LGUI, MO(_LO), KC_SPC,  KC_ENT,  MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2, _______, _______,  _______
     ),
 
     [_RA] = LAYOUT_ortho_5x15( /* FUNCTION */
-        _______, _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-        _______, _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______,  _______,  KC_CAPS, KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC,  KC_ENT,  MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,  _______,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,  _______,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, _______, _______,  _______,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,  _______,
+        KC_CAPS, KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC,  KC_ENT,  MO(_RA), KC_RGUI, KC_RALT, KC_BTN1, KC_BTN2, _______, _______,  _______
     )
 
 };
