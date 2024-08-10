593726c1e3fbcdb6fee40f37e740c4f8cfbf8e02
Aldiantoro Nugroho <kriwil@gmail.com>
2024-08-11 06:08:17 +0700

    change layer display to be uppercase

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -13,17 +13,17 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Base");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: BASE");
     break;
   case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RAISE");
     break;
   case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: LOWER");
     break;
   case L_ADJUST:
   case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
