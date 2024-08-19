00e026623a59099499e0a0cbb7036bc86e3ceda0
Aldiantoro Nugroho <kriwil@gmail.com>
2024-08-19 15:19:06 +0700

    fix legend for sofle

--- keyboards/sofle/keymaps/kriwil/keymap.c
+++ keyboards/sofle/keymaps/kriwil/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSPC,
   KC_LCTL,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,   KC_O,    KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, XXXXXXX, KC_K,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, KC_MPLY, KC_K,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                     KC_DEL,  KC_LALT, KC_LGUI, TL_LOWR, KC_SPC,  KC_ENT,  TL_UPPR, KC_RGUI, KC_RALT, KC_RCTL
 ),
 /* LOWER
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |CPSLCK|
  * |------+------+------+------+------+------|-------|    |-------|------+------+------+------+------+------|
- * |LSHIFT|  =   |  -   |  +   |   {  |   }  |  MUTE |    |       |   [  |   ]  |   _  |   |  |   \  |RSHIFT|
+ * |LSHIFT|  =   |  -   |  +   |  [   |   ]  |  MUTE |    | PLAY  |   {  |   }  |   _  |   |  |   \  |RSHIFT|
  * `-----------------------------------------|-------|    |-------|-----------------------------------------'
  *               | DEL  | LALT | LGUI |LOWER | SPACE |    | ENTER |RAISE | RGUI | RALT |RCTRL |
  *               `----------------------------------'     '-----------------------------------'
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |      | DEL  | END  |PGDOWN|      |                    |      | LEFT | DOWN |RIGHT |      |      |
  * |------+------+------+------+------+------|-------|    |-------|------+------+------+------+------+------|
- * |LSHIFT|     |       |      |      |      |  MUTE |    |       |      |      |      |      |      |RSHIFT|
+ * |LSHIFT|     |       |      |      |      |  MUTE |    | PLAY  |      |      |      |      |      |RSHIFT|
  * `-----------------------------------------|-------|    |-------|-----------------------------------------'
  *               | DEL  | LALT | LGUI |LOWER | SPACE |    | ENTER |RAISE | RGUI | RALT |RCTRL |
  *               `----------------------------------'     '-----------------------------------'
