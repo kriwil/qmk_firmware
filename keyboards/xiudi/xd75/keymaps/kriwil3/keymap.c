2a32d2e8601977c8b636dbbfb440a98f8ac007a1
Aldiantoro Nugroho <kriwil@gmail.com>
2024-10-02 07:23:38 +0700

    fix typo

--- keyboards/xiudi/xd75/keymaps/kriwil3/keymap.c
+++ keyboards/xiudi/xd75/keymaps/kriwil3/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * '--------------------------------------------------------------------------------------------------------------------------------------'
      */
     [_COS] = LAYOUT_ortho_5x15( /* COLEMAK SPLIT */
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     XXXXXXX, KC_MUTE,   XXXXXXX, KC_6,   KC_7,    KC_8,    KC_9,    KC_10,   KC_GRV,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     XXXXXXX, KC_MUTE,   XXXXXXX, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,     XXXXXXX, KC_MPLY,   XXXXXXX, KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
         KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,     XXXXXXX, XXXXXXX,   XXXXXXX, KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     XXXXXXX, XXXXXXX,   XXXXXXX, KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
