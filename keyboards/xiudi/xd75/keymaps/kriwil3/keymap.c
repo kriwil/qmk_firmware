020ac9fa5f74942f99b581f2d542f1665a0ffc57
Aldiantoro Nugroho <kriwil@gmail.com>
2024-10-02 07:21:41 +0700

    new layout

--- keyboards/xiudi/xd75/keymaps/kriwil3/keymap.c
+++ keyboards/xiudi/xd75/keymaps/kriwil3/keymap.c
@@ -18,6 +18,7 @@
 
 enum layer_names {
     _COS,
+    _COSM,
     _LO,
     _RA
 };
@@ -37,11 +38,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * '--------------------------------------------------------------------------------------------------------------------------------------'
      */
     [_COS] = LAYOUT_ortho_5x15( /* COLEMAK SPLIT */
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_MUTE, KC_F5,     KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,     KC_7,    KC_MPLY, KC_F4,     KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,     KC_8,    XXXXXXX, KC_F3,     KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_9,    XXXXXXX, KC_F2,     KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_CAPS, KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC,   KC_0,    XXXXXXX, KC_F1,     KC_ENT, MO(_RA), KC_LGUI, KC_RALT, KC_BTN1, KC_BTN2
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     XXXXXXX, KC_MUTE,   XXXXXXX, KC_6,   KC_7,    KC_8,    KC_9,    KC_10,   KC_GRV,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,     XXXXXXX, KC_MPLY,   XXXXXXX, KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,     XXXXXXX, XXXXXXX,   XXXXXXX, KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     XXXXXXX, XXXXXXX,   XXXXXXX, KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_CAPS, KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC,   XXXXXXX, DF(_COSM), XXXXXXX, KC_ENT, MO(_RA), KC_LGUI, KC_RALT, KC_BTN1, KC_BTN2
+    ),
+    [_COSM] = LAYOUT_ortho_5x15( /* COLEMAK SPLIT FOR MINECRAFT */
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_MUTE,   KC_F5,   KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_GRV,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,     KC_7,    KC_MPLY,   KC_F4,   KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,     KC_8,    XXXXXXX,   KC_F3,   KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_9,    XXXXXXX,   KC_F2,   KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_CAPS, KC_DEL,  KC_LALT, KC_LGUI, MO(_LO), KC_SPC,   KC_0,    DF(_COS),  KC_F1,   KC_ENT, MO(_RA), KC_LGUI, KC_RALT, KC_BTN1, KC_BTN2
     ),
 
     /* LOWER
