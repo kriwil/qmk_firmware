119ac201df05a4fa57a507dc7ff02b0deccdc397
Aldiantoro Nugroho <kriwil@gmail.com>
2025-01-30 12:33:52 +0700

    lily58 with corne layout

--- keyboards/lily58/keymaps/kriwil/keymap.c
+++ keyboards/lily58/keymaps/kriwil/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                      `---------------------------'     '----------------------------'
  */
  [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_BSPC,
-  KC_LCTL,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                     KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE, KC_MPLY,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        XXXXXXX, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RALT, XXXXXXX
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_TAB,  KC_Q,     KC_W,   KC_F,    KC_P,    KC_G,                      KC_J,       KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+  KC_LCTL, KC_A,     KC_R,   KC_S,    KC_T,    KC_D,                      KC_H,       KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_LSFT, KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,    XXXXXXX, XXXXXXX, KC_K,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                        XXXXXXX, KC_LGUI, MO(_LOWER),   KC_SPC,  KC_ENT,  MO(_RAISE), KC_RALT, XXXXXXX
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -38,37 +38,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |CPSLCK|
  * |------+------+------+------+------+------|-------|    |-------|------+------+------+------+------+------|
- * |LSHIFT|  =   |  -   |  +   |   [  |   ]  |  MUTE |    | PLAY  |   {  |   }  |   _  |   |  |   \  |RSHIFT|
+ * |LSHIFT|  =   |  -   |  +   |   [  |   ]  |       |    |       |   {  |   }  |   _  |   |  |   \  |RSHIFT|
  * `-----------------------------------------|-------|    |-------|-----------------------------------------'
  *                      |      | LGUI |LOWER | SPACE |    | ENTER |RAISE | RALT |      |
  *                      `---------------------------'     '----------------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_CAPS,
-  _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, _______, _______, KC_LCBR, KC_RCBR, KC_UNDS, KC_PIPE, KC_BSLS, _______,
+  _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_UNDS, KC_PIPE, KC_BSLS, _______,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |      | INS  | HOME | PGUP |      |                    |      |      | UP   |      |      |      |
+ * | ESC  |      | INS  | HOME | PGUP |      |                    |      |      | UP   |      |      |VOLUP |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |      | DEL  | END  |PGDOWN| MUTE |                    | PLAY | LEFT | DOWN |RIGHT |      |      |
+ * |LCTRL |      | DEL  | END  |PGDOWN| MUTE |                    | PLAY | LEFT | DOWN |RIGHT |      |VOLDN |
  * |------+------+------+------+------+------|-------|    |-------|------+------+------+------+------+------|
- * |LSHIFT|     |       |      |VOLDN |VOLUP |  MUTE |    | PLAY  |      |      |      |      |      |RSHIFT|
+ * |LSHIFT|     |       |      |       |     |       |    |       |      |      |      |      |      |RSHIFT|
  * `-----------------------------------------|-------|    |-------|-----------------------------------------'
  *                      |      | LGUI |LOWER | SPACE |    | ENTER |RAISE | RALT |      |
  *                      `---------------------------'     '----------------------------'
  */
 [_RAISE] = LAYOUT(
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_ESC,  XXXXXXX, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, KC_DEL,  KC_END,  KC_PGDN, KC_MUTE,                   KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_ESC,  XXXXXXX, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_VOLU,
+  _______, KC_F11,  KC_DEL,  KC_END,  KC_PGDN, KC_MUTE,                   KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, KC_F12,  KC_VOLD,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+                             XXXXXXX, _______, _______, _______, _______,  _______, _______, XXXXXXX
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
