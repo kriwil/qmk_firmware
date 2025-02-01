42fe3f644336a37d402bc4390c95fb92b47738dc
Aldiantoro Nugroho <kriwil@gmail.com>
2025-02-01 16:14:42 +0700

    increase tapping term

--- keyboards/lily58/keymaps/kriwil/config.h
+++ keyboards/lily58/keymaps/kriwil/config.h
@@ -27,8 +27,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 #define HOLD_ON_OTHER_KEY_PRESS
+#define PERMISSIVE_HOLD
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
