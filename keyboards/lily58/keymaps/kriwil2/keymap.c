7ce469dbffc96ddd0764de58019dc9d2b0553ead
Aldiantoro Nugroho <kriwil@gmail.com>
2025-02-01 19:31:17 +0700

    move esc

--- keyboards/lily58/keymaps/kriwil2/keymap.c
+++ keyboards/lily58/keymaps/kriwil2/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCRTL |   A  |   R  |   S  |   T  |   D  |                    |   H  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|-------|    |-------|------+------+------+------+------+------|
- * |LSHIFT|   Z  |   X  |   C  |   V  |   B  |       |    |       |   K  |   M  |   ,  |   .  |   /  |RSHIFT|
+ * |LSHIFT|   Z  |   X  |   C  |   V  |   B  |       |    |       |   K  |   M  |   ,  |   .  |   /  | ESC  |
  * `-----------------------------------------|-------|    |-------|-----------------------------------------'
   *                     |      | LGUI |LOWER | SPACE |    | ENTER |RAISE | RALT |      |
  *                      `---------------------------'     '----------------------------'
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_TAB,  KC_Q,     KC_W,   KC_F,    KC_P,    KC_G,                      KC_J,       KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
   KC_LCTL, KC_A,     KC_R,   KC_S,    KC_T,    KC_D,                      KC_H,       KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_LSFT, KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,    XXXXXXX, XXXXXXX, KC_K,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  KC_LSFT, KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,    XXXXXXX, XXXXXXX, KC_K,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
                         XXXXXXX, KC_LGUI, MO(_LOWER),   KC_SPC,  KC_ENT,  MO(_RAISE), KC_RALT, XXXXXXX
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BSPC |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |ENTER |
  * |------+------+------+------+------+------|-------|    |-------|------+------+------+------+------+------|
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_ENT,
-  _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_UNDS, KC_PIPE, KC_BSLS, _______,
+  _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_UNDS, KC_PIPE, KC_BSLS, KC_RSFT,
                              XXXXXXX, _______, _______, _______, _______,  _______, _______, XXXXXXX
 ),
 /* RAISE
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_ESC,  XXXXXXX, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_VOLU,
+  _______, XXXXXXX, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_VOLU,
   _______, KC_F11,  KC_DEL,  KC_END,  KC_PGDN, KC_MUTE,                   KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, KC_F12,  KC_VOLD,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
                              XXXXXXX, _______, _______, _______, _______,  _______, _______, XXXXXXX
